3fb10756bd5c5468c87c93b94fbfc8a4b0980885
jerry08 <jeremyyearwood99@gmail.com>
2022-10-11 17:02:43 -0400

    Upload

---
+++ AniStream/Utils/AppUpdater.cs
@@ -0,0 +1,17 @@
+﻿using AndroidX.Fragment.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniStream.Utils;
+
+internal class AppUpdater
+{
+    public void Check(FragmentActivity activity)
+    {
+        var repo = activity.GetString(Resource.String.repo);
+
+    }
+}
