db668109ea39062a7ef29cd323bd9a0e7798cd29
jerrry08 <jeremyyearwood99@gmail.com>
2023-10-30 14:50:44 -0400

    Add bottom sheet

---
+++ AniStream/Views/BottomSheets/CoverImageSheet.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Berry.Maui;
+using Microsoft.Maui;
+using Microsoft.Maui.Devices;
+
+namespace AniStream.Views.BottomSheets;
+
+public partial class CoverImageSheet
+{
+    public CoverImageSheet()
+    {
+        InitializeComponent();
+
+        var statusBarHeight =
+            ApplicationEx.GetStatusBarHeight() / DeviceDisplay.MainDisplayInfo.Density;
+        MainGrid.Margin = new Thickness(5, statusBarHeight + 10, 5, 0);
+    }
+}
