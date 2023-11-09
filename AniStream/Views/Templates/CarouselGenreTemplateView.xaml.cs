e5e637aea28c02eb04f05693a15c7ea71740c2d9
jerrry08 <jeremyyearwood99@gmail.com>
2023-11-08 22:49:40 -0400

    Improve UI

---
+++ AniStream/Views/Templates/CarouselGenreTemplateView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace AniStream.Views.Templates;
+
+public partial class CarouselGenreTemplateView
+{
+    public CarouselGenreTemplateView()
+    {
+        InitializeComponent();
+    }
+}
