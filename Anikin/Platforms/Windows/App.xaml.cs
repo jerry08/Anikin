e7b7c79adadaa70bab36864d9c036c2465b55ab4
jerrry08 <jeremyyearwood99@gmail.com>
2024-01-19 08:48:24 -0400

    Catch unhandled exceptions for windows

--- Anikin/Platforms/Windows/App.xaml.cs
+++ Anikin/Platforms/Windows/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Maui;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
-using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,7 +21,32 @@ public partial class App : MauiWinUIApplication
     public App()
     {
         this.InitializeComponent();
+
+        UnhandledException += App_UnhandledException;
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    private void App_UnhandledException(
+        object sender,
+        Microsoft.UI.Xaml.UnhandledExceptionEventArgs e
+    )
+    {
+        e.Handled = true;
+
+        try
+        {
+            //var path = Path.Join(Environment.CurrentDirectory, "error_log.txt");
+            var path = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "error_log.txt");
+            File.AppendAllText(
+                path,
+                $"{DateTime.Now:F}{Environment.NewLine}{e.Exception}{Environment.NewLine}{Environment.NewLine}"
+            );
+        }
+        catch { }
+
+        Debug.WriteLine("Exception catched");
+
+        Anikin.App.AlertService.ShowAlert("Error", $"{e.Exception}");
+    }
 }
