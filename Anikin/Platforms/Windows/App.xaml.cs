cd295e985f0a102b4ae65128c4e47b8aab13d38c
jerry08 <jeremyyearwood99@gmail.com>
2024-08-04 18:28:26 -0400

    Begin windows login with anilist

--- Anikin/Platforms/Windows/App.xaml.cs
+++ Anikin/Platforms/Windows/App.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
+using Anikin.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,6 +27,52 @@ public partial class App : MauiWinUIApplication
         this.InitializeComponent();
 
         UnhandledException += App_UnhandledException;
+    }
+
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        base.OnLaunched(args);
+
+        var mainInstance = Microsoft.Windows.AppLifecycle.AppInstance.FindOrRegisterForKey("main");
+
+        if (!mainInstance.IsCurrent)
+        {
+            var activatedArgs = Microsoft
+                .Windows.AppLifecycle.AppInstance.GetCurrent()
+                .GetActivatedEventArgs();
+
+            if (
+                activatedArgs.Data
+                is Windows.ApplicationModel.Activation.ProtocolActivatedEventArgs token
+            )
+            {
+                //var slicedToken = SlicedToken(token.Uri.ToString());
+
+                var settingsService = Services.GetRequiredService<SettingsService>();
+                settingsService.Load();
+
+                var regex = new Regex("""(?<=access_token=).+(?=&token_type)""");
+                var tokenStr = regex.Match(token.Uri.ToString()).Value;
+
+                if (!string.IsNullOrWhiteSpace(tokenStr))
+                {
+                    settingsService.Load();
+                    settingsService.AnilistAccessToken = tokenStr;
+                    settingsService.Save();
+                }
+
+                await mainInstance.RedirectActivationToAsync(activatedArgs);
+                Process.GetProcessById((int)mainInstance.ProcessId).Kill();
+            }
+        }
+    }
+
+    private string SlicedToken(string rawToken)
+    {
+        //var token = rawToken.Replace("animoe:/auth#access_token=", "");
+        var token = rawToken.Replace("anistream://anilist/#access_token=", "");
+        token = token.Split('&')[0];
+        return token;
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
